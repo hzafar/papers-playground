297bf50ee1468ca2b393ad98b08c31dad62c5273
Huma Zafar <zafar.huma@gmail.com>
2018-06-13 02:49:01 +0000

    Correctly enforce linearity in inference rules; fix misc bugs.

--- llc/tests.rkt
+++ llc/tests.rkt
@@ -9,48 +9,52 @@
 ;;->>--->>--------->>--------------------------------------------------------------------------------->>
 ;; Tests
 
+(define (check-type Γ Δ e expected-type [expected-resources empty])
+  (let-values ([(t Δ-) (infer Γ Δ e)])
+    (and (equal? Δ- expected-resources)
+         (equal? t expected-type))))
+
 ; ------------------------------------------------------------------------------
 (define linctx1 (list (cons 'x B) (cons 'y B) (cons 'z Z)))
 (define linctx2 (list (cons 'x Z)))
 (define intctx1 (list (cons 'v B)))
 
 ; v:B ; . ⊢ v:B
-(check-true (typecheck intctx1 empty (var 'v) B))
+(check-true (check-type intctx1 empty (var 'v) B))
 
 ; . ; . !⊢ x:B
-(check-false (typecheck empty empty (var 'x) B))
+(check-false (check-type empty empty (var 'x) B))
 
 ; . ; x:B, y:B, z:Z !⊢ x:B
-(check-false (typecheck empty linctx1 (var 'x) B))
+(check-false (check-type empty linctx1 (var 'x) B))
 
 ; . ; x:Z ⊢ x:Z
-(check-true (typecheck empty linctx2 (var 'x) Z))
+(check-true (check-type empty linctx2 (var 'x) Z))
 
 ; ------------------------------------------------------------------------------
 (define intctx2 (list (cons 'y Z)))
 (define linctx3 (list (cons 'x B)))
 
-(check-true (typecheck empty empty (λlin (var 'x) B (var 'x)) (LinArr B B)))
+(check-true (check-type empty empty (λlin (var 'x) B (var 'x)) (LinArr B B)))
 
-(check-false (typecheck empty linctx3 (λlin (var 'w) B (var 'w)) (LinArr B B)))
+(check-false (check-type empty linctx3 (λlin (var 'w) B (var 'w)) (LinArr B B)))
 
-(check-exn exn:fail?
-  (lambda () (typecheck intctx2 empty (λlin (var 'x) B (⊗ (var 'y) (var 'y)))
-                        (LinArr B (Tensor Z Z)))))
+(check-false (check-type intctx2 empty (λlin (var 'x) B (⊗ (var 'y) (var 'y)))
+                         (LinArr B (Tensor Z Z))))
 
 ; ------------------------------------------------------------------------------
 (define linctx4 (list (cons 'x Z)))
 (define linctx5 (list (cons 'x B)))
 
-(check-false (typecheck empty linctx4 (linapply (λlin (var 'y) B (var 'y)) (var 'x)) B))
-(check-true (typecheck empty linctx5 (linapply (λlin (var 'y) B (var 'y)) (var 'x)) B))
+(check-false (check-type empty linctx4 (linapply (λlin (var 'y) B (var 'y)) (var 'x)) B))
+(check-true (check-type empty linctx5 (linapply (λlin (var 'y) B (var 'y)) (var 'x)) B))
 
 ; ------------------------------------------------------------------------------
 ; Not sure if I understand these ones fully tbh
 (define intctx3 (list (cons 'x Z)))
 
-(check-true (typecheck intctx3 empty (! (var 'x)) (Valid Z)))
-(check-true (typecheck intctx3 empty (case! (! (var 'x)) 'x (var 'x)) Z))
+(check-true (check-type intctx3 empty (! (var 'x)) (Valid Z)))
+(check-true (check-type intctx3 empty (case! (! (var 'x)) 'x (var 'x)) Z))
 
 ; ------------------------------------------------------------------------------
 (define linctx6 (list (cons 'thing1 Z) (cons 'thing2 B)))
@@ -58,45 +62,43 @@
 (define intctx4 (list (cons 'u Z)))
 (define intctx5 (list (cons 'v B)))
 
-(check-true (typecheck empty linctx6 (⊗ (var 'thing1) (var 'thing2)) (Tensor Z B)))
-(check-true (typecheck intctx4 linctx7 (⊗ (var 'u) (var 'v)) (Tensor Z B)))
-(check-true (typecheck intctx5 empty (⊗ (var 'v) (var 'v)) (Tensor B B)))
-(check-false (typecheck empty linctx7 (⊗ (var 'v) (var 'v)) (Tensor B B)))
+(check-true (check-type empty linctx6 (⊗ (var 'thing1) (var 'thing2)) (Tensor Z B)))
+(check-true (check-type intctx4 linctx7 (⊗ (var 'u) (var 'v)) (Tensor Z B)))
+(check-true (check-type intctx5 empty (⊗ (var 'v) (var 'v)) (Tensor B B)))
+(check-false (check-type empty linctx7 (⊗ (var 'v) (var 'v)) (Tensor B B)))
 
 ; ------------------------------------------------------------------------------
 (define linctx8 (list (cons 'x B) (cons 'y Z)))
 
-(check-exn exn:fail?
-  (lambda () (typecheck empty linctx8 (case⊗ (⊗ (var 'x) (var 'y)) 'p1 'p2 (var 'p1)) B)))
+(check-false (check-type empty linctx8 (case⊗ (⊗ (var 'x) (var 'y)) 'p1 'p2 (var 'p1)) B))
 
 ; ------------------------------------------------------------------------------
 (define linctx9 (list (cons 'x Z)))
 
-(check-true (typecheck empty linctx9 (& (var 'x) (var 'x)) (Ampersand Z Z)))
+(check-true (check-type empty linctx9 (& (var 'x) (var 'x)) (Ampersand Z Z)))
 
 ; ------------------------------------------------------------------------------
 (define intctx6 (list (cons 'x B) (cons 'y Z)))
 (define linctx10 (list (cons 'w Z) (cons 'u B)))
 
-(check-true (typecheck intctx6 empty (fst (& (var 'x) (var 'y))) B))
-(check-true (typecheck intctx6 empty (snd (& (var 'x) (var 'y))) Z))
-(check-true (typecheck empty linctx10 (fst (& (⊗ (var 'w) (var 'u)) (⊗ (var 'u) (var 'w))))
-                       (Tensor Z B)))
+(check-true (check-type intctx6 empty (fst (& (var 'x) (var 'y))) B))
+(check-true (check-type intctx6 empty (snd (& (var 'x) (var 'y))) Z))
+(check-true (check-type empty linctx10 (fst (& (⊗ (var 'w) (var 'u)) (⊗ (var 'u) (var 'w))))
+                        (Tensor Z B)))
 
 ; ------------------------------------------------------------------------------
 (define linctx11 (list (cons 'x B)))
 
-(check-true (typecheck empty linctx11 (inl Z (var 'x)) (Either B Z)))
-(check-true (typecheck empty linctx11 (inr Z (var 'x)) (Either Z B)))
+(check-true (check-type empty linctx11 (inl Z (var 'x)) (Either B Z)))
+(check-true (check-type empty linctx11 (inr Z (var 'x)) (Either Z B)))
 
 ; ------------------------------------------------------------------------------
 (define intctx7 (list (cons 'w Z)))
 (define linctx12 (list (cons 'x B)))
-(check-exn exn:fail?
-  (lambda() (typecheck intctx7 linctx12 
-                       (case⊕ (inr B (var 'x)) 
-                         'l (linapply (λlin (var 'x) B (var 'x)) (var 'l))
-                         'r (var 'w))
-                       Z)))
+(check-false (check-type intctx7 linctx12 
+                         (case⊕ (inr B (var 'x)) 
+                           'l (linapply (λlin (var 'x) B (var 'x)) (var 'l))
+                           'r (var 'w))
+                         Z))
 
 (test-log #:display? #t #:exit? #t)
--- llc/typechecker.rkt
+++ llc/typechecker.rkt
@@ -1,25 +1,24 @@
 #lang racket
 
-(require "llc.rkt")
+(require
+  "llc.rkt"
+  racket/set)
 
 (provide (all-defined-out))
 
 ;;->>--->>--------->>--------------------------------------------------------------------------------->>
 ;; Inference functions
 
-; Using #f to indicate type inference failure.
+; This macro is invoked by the functions below whenever a term cannot be typed.
+; It produces #f as the "type" to indicate failure.
 (define-syntax ☠ (λ (stx) (syntax-case stx () [_ #'(values #f empty)])))
 
-; Each of the inference functions defined here produce two values: a type (or #f),
-; and a linear context that contains all of the still unconsumed resources.
-; Thus, for a term to typecheck, 1) the type returned must not be #f, and 2) the
-; linear context must ultimately be empty.
-
-(define (typecheck Γ Δ e expected)
-  (let-values ([(t Δ-) (infer Γ Δ e)])
-    (and (empty? Δ-) (equal? t expected))))
-
-
+; Each of the inference functions defined here produces two values: a type (or #f),
+; and a linear context that contains all of the still unconsumed resources. Locally,
+; each rule enforces singular usage of variables where appropriate, and checks that
+; variables introduced by a form are really used. For example, see `infer-λ-lin`,
+; which will fail typechecking for a lambda that does not use its argument.
+;
 ; The idea for the inferences follows the main part of what's described in section
 ; 6.2 of https://www.cs.cmu.edu/~fp/courses/linear/handouts/linear.pdf. Each function
 ; consumes two contexts: Γ and Δ. Γ contains the unrestricted (intuitionistic)
@@ -28,13 +27,12 @@
 ; For multiplicative connectives, Δ must be split amongst the premises, so rather
 ; than non-deterministically determining this split we first pass all the
 ; hypotheses to the first premise, then the leftover ones to the next premise,
-; and so on. If the term is well-typed, we should end up with no linear
-; hypotheses unconsumed.
+; and so on. The final remaining hypotheses are returned along with the inferred
+; type. (At the top level, the final linear context should of course be empty.)
 ;
-; For the additive connectives, the premises must be checked in the same
-; context, so we pass Δ whole to each (parallel) premise. Note that the complication
-; with checking the ⊕ unit that the above book mentions thankfully doesn't matter
-; here as Wadler's LLC doesn't contain such a term. :]
+; For the additive connectives, the sub-terms must be checked in the same linear
+; context as the term itself, so we pass Δ whole to each recursive call to
+; `infer`, and return the intersection of all the resulting leftover contexts.
 
 (define (infer Γ Δ expr)
   (match expr
@@ -72,34 +70,41 @@
 (define (infer-λ-lin Γ Δ x T b)
   (let-values ([(T2 Δ1) (infer Γ (cons (cons x T) Δ) b)])
     (if (member x (map car Δ1))
-        (error 'infer-λ-lin "Unused argument ~a" x)
-        (if T2 (values (LinArr T T2) Δ) ☠))))
+        ☠ ; argument x is unused!
+        (if T2 (values (LinArr T T2) Δ1) ☠)))) 
+
+; If the body of the lambda leaves the argument unconsumed, we signal failure,
+; because nothing else has access to that argument (and so cannot possibly
+; consume it) so we'll ultimately be left with an unused linear variable.
 
 ; ..............................................................................
 (define (infer-linapply Γ Δ e1 e2)
   (let*-values ([(Tapp Δ1) (infer Γ Δ e1)]
                 [(Targ Δ2) (infer Γ Δ1 e2)])
-    (if (and Tapp Targ (LinArr? Tapp) (equal? (LinArr-Arg Tapp) Targ))
-        (values (LinArr-Result Tapp) empty)
+    (if (and Tapp (LinArr? Tapp) Targ (equal? (LinArr-Arg Tapp) Targ))
+        (values (LinArr-Result Tapp) Δ2)
         ☠)))
 
 ; ..............................................................................
 (define (infer-valid Γ Δ e)
   (let-values ([(T Δ1) (infer Γ empty e)])
-    (if T (values (Valid T) Δ) ☠)))
+    (if T (values (Valid T) Δ1) ☠))) 
 
 ; The ! connective is related to "validity" from modal logic, where we said a
 ; proposition was valid if it could be inferred using only valid hypotheses.
-; Here, "valid" means it consumes only unrestricted hypotheses (see the empty
-; passed in as the linear context to infer).
+; Here, "valid" means it consumes only unrestricted hypotheses (see the `empty`
+; passed in as the linear context to `infer`).
 
 ; ..............................................................................
 (define (infer-valid-case Γ Δ s x u)
   (let-values ([(T Δ1) (infer Γ Δ s)])
     (if (and T (Valid? T))
         (let-values ([(B Δ2) (infer (cons (cons x (Valid-A T)) Γ) Δ1 u)])
-          (if (and B (empty? Δ2)) (values B Δ2) ☠))
+          (if B (values B Δ2) ☠)) 
         ☠)))
+
+; Notice that we don't care if the x we add to Γ is used or not -- because it's
+; an unrestricted hypothesis.
 
 ; ..............................................................................
 (define (infer-tensor Γ Δ e1 e2)
@@ -117,7 +122,7 @@
                       [(B) (Tensor-B T1)]
                       [(C Δ2) (infer Γ (append (list (cons p1 A) (cons p2 B)) Δ1) v)])
           (if (or (member p1 (map car Δ2)) (member p2 (map car Δ2)))
-              (error 'infer-tensor-case "Unused arguments ~a or ~a" p1 p2)
+              ☠ ; arguments p1 and p2 are not both used!
               (if C (values C Δ2) ☠)))
         ☠)))
 
@@ -126,7 +131,7 @@
   (let-values ([(T1 Δ1) (infer Γ Δ e1)]
                [(T2 Δ2) (infer Γ Δ e2)])
     (if (and T1 T2)
-        (values (Ampersand T1 T2) (append Δ1 Δ2))
+        (values (Ampersand T1 T2) (list-intersect Δ1 Δ2))
         ☠)))
 
 ; ..............................................................................
@@ -157,14 +162,15 @@
 (define (infer-either-case Γ Δ s x v y w)
   (let-values ([(T Δ1) (infer Γ Δ s)])
     (if (and T (Either? T))
-        (let-values ([(Cl Δ1) (infer Γ (cons (cons x (Either-A T)) Δ1) v)]
-                     [(Cr Δ2) (infer Γ (cons (cons y (Either-B T)) Δ1) w)])
+        (let-values ([(Cl Δ2) (infer Γ (cons (cons x (Either-A T)) Δ1) v)]
+                     [(Cr Δ3) (infer Γ (cons (cons y (Either-B T)) Δ1) w)])
           (if (or (member x (map car Δ1)) (member y (map car Δ2)))
-              (error 'infer-either-case "Unused arguments ~a or ~a" x y)
-              (if (and Cl Cr (equal? Cl Cr) (empty? Δ1) (empty? Δ2))
-                  (values Cl empty)
+              ☠ ; arguments x and y are not both used in their respective branches!
+              (if (and Cl Cr (equal? Cl Cr))
+                  (values Cl (list-intersect Δ2 Δ3))
                   ☠)))        
         ☠)))
 
-; Note that this rule initially splits the context to typecheck s, but then has
-; to check both branches using the same linear context.
+
+(define (list-intersect l1 l2)
+  (set->list (set-intersect (list->set l1) (list->set l2))))
